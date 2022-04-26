8ac8f0a888c29b900a084e398ec19f2b8e7bfb84
ShotaMiyahara <smiyahara@landscape.co.jp>
2022-04-26 17:50:42 +0900

    ✨ チャット機能作成

--- src/app/shared/firestore.service.ts
+++ src/app/shared/firestore.service.ts
@@ -1,19 +1,59 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/compat/firestore';
-import { first } from 'rxjs/operators';
+import {
+  AngularFirestore,
+  AngularFirestoreDocument,
+  AngularFirestoreCollection,
+} from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
+import { first, concatMap } from 'rxjs/operators';
 
 export interface IUser {
   displayName: string;
   photoDataUrl: string;
 }
 
+export interface IMessage {
+  uid: string;
+  message: string;
+  timeStamp: number;
+}
+
+export interface IChat extends IUser, IMessage {}
+
 @Injectable({
   providedIn: 'root',
 })
 export class FirestoreService {
   userDoc: AngularFirestoreDocument<IUser>;
+  messageCollection: AngularFirestoreCollection<IMessage>;
+  userCollection: AngularFirestoreCollection<IUser>;
 
-  constructor(public af: AngularFirestore) {}
+  constructor(public af: AngularFirestore) {
+    this.messageCollection = this.af.collection<IMessage>('chat', (ref) =>
+      ref.orderBy('timeStamp', 'desc'),
+    );
+    this.userCollection = this.af.collection<IUser>('users');
+  }
+
+  messageAdd(message: IMessage) {
+    return this.messageCollection.add(message);
+  }
+
+  chatInit(): Observable<IChat[]> {
+    return this.messageCollection.valueChanges({ idField: 'messageId' }).pipe(
+      concatMap(async (messages) => {
+        const users = await this.userCollection
+          .valueChanges({ idField: 'uid' })
+          .pipe(first())
+          .toPromise(Promise);
+
+        return messages.map((message) => {
+          const user = users.find((u) => u.uid === message.uid);
+          return Object.assign(message, user);
+        });
+      }),
+    );
+  }
 
   userInit(uid: string): Promise<IUser> {
     this.userDoc = this.af.doc<IUser>(`users/${uid}`);
